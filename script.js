df47d2cb967d7308f2d849981f251bf5aa7020cd
Sharath Nirmala <sharathnimala@gmail.com>
2021-09-13 09:38:48 +0000

    Final website

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+//Computes the result
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var principalElement = document.getElementById("principal");
+    if (principal <= 0) 
+    {
+        alert("Please enter a positive number.");
+        principalElement.focus();
+    }
+    else
+    {
+        document.getElementById("result").innerHTML="\<br\>If you deposit "+
+        principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+
+        interest+",\<br\>in the year "+year+".\<br\>";
+    }
 }
-        
+
+//Outputs the final result
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
